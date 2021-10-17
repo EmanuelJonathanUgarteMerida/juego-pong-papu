1741284a63f7fc7e591f4de59072fd1bc6783644
Emanuel Jonathan Ugarte Mérida <emanuelugartemerida@gmail.com>
2021-10-17 14:22:13 +0200

    constantes

---
+++ configuracion.py
@@ -0,0 +1,21 @@
+    
+# constantes juego
+_JUEGO_VENTANA_NOMBRE = 'The REAL PONG RCTM!'
+# constantes pantalla
+_PANTALLA_ALTURA = 480
+_PANTALLA_ANCHO = 720
+_PANTALLA_MARGEN_LATERAL = 40
+pantalla_centro_h = _PANTALLA_ANCHO/2
+pantalla_centro_v = _PANTALLA_ALTURA/2
+_PANTALLA_CLOCK = pygame.time.Clock()
+
+# constantes paleta
+_PALETA_ANCHO = 10
+_PALETA_ALTURA = 40
+_VELOCIDAD = 5
+paleta_centro_v = pantalla_centro_v-_PALETA_ALTURA/2
+
+# constantes bola
+_BOLA_DIMEN = 20
+pelota_centro_v = pantalla_centro_v-_BOLA_DIMEN/2
+pelota_centro_h = pantalla_centro_h-_BOLA_DIMEN/2

3f0aaa91f368063d5751da3e320561d487ac2452
Emanuel Jonathan Ugarte Mérida <92303813+EmanuelJonathanUgarteMerida@users.noreply.github.com>
2021-10-20 16:11:34 +0200

    Selección de 1 - 2 jugadores. Sin efecto en juego
    Se inicia control de jugadores, para seleccionar 1 - 2 jugadores, al seleccionar mediante las teclas correspondientes (1 ó 2), se marcará el número de jugadores seleccionado. Aún sin efecto alguno sobre el juego

--- pong.py
+++ pong.py
@@ -12,12 +12,13 @@ class Pong:
     def __init__(self):
         pygame.init()
         self.reloj = pygame.time.Clock()
-        self.pantalla = pygame.display.set_mode(
-            (cf._PANTALLA_ANCHO, cf._PANTALLA_ALTURA))
+        self.pantalla = pygame.display.set_mode((cf._PANTALLA_ANCHO, cf._PANTALLA_ALTURA))
         pygame.display.set_caption(cf._JUEGO_VENTANA_NOMBRE)
         pygame.font.init()
         self.ganador = ''
         self.hay_ganador = False
+        self.empezar = False		
+        self.jugadores=0
 
         self.jugador1 = paleta.Paleta(
             'Jugador1', (pygame.K_a, pygame.K_z),
@@ -32,6 +33,7 @@ class Pong:
         self.bola = bola.Bola(cf._VELOCIDAD, cf._BOLA_DIMEN)
 
         self.marcador = marcador.Marcador(cf._COLOR_TXT_MARCADOR)
+		
 
     def colision_paleta(self):
         if self.jugador1.colliderect(self.bola) or self.jugador2.colliderect(self.bola):
@@ -67,30 +69,63 @@ class Pong:
                 if evento.type == pygame.KEYDOWN:
                     if evento.key == pygame.K_ESCAPE:
                         return
+                    elif evento.key == pygame.K_SPACE:
+                        if self.jugadores != 0:
+                            self.empezar=True
+                    elif evento.key in (pygame.K_1 ,pygame.K_KP1):
+                        self.jugadores=1
+                    elif evento.key in (pygame.K_2,pygame.K_KP2):
+                        self.jugadores=2
 
-             # se controla si hay un ganador
             self.pantalla.fill(cf._NEGRO)
-            if self.hay_ganador:
-                self.pantalla.blit(
-                    self.ganador, (cf._PANTALLA_ANCHO/2-self.ganador.get_width()/2, cf._PANTALLA_ALTURA/2-self.ganador.get_height()/2))
+            if self.empezar:
+				 # se controla si hay un ganador
+                
+                if self.hay_ganador:
+                    self.pantalla.blit(self.ganador, (cf._PANTALLA_ANCHO/2-self.ganador.get_width()/2, cf._PANTALLA_ALTURA/2-self.ganador.get_height()/2))
+                else:
+                    self.jugador1.muevete()
+                    self.jugador2.muevete()
+                    self.bola.muevete()
+
+                    self.colision_paleta()
+                    self.colision_bordes()
+
+                    self.pantalla.blit(self.marcador.pintar((1, self.jugador1.puntos)), self.marcador.posicion)
+                    self.pantalla.blit(self.marcador.pintar((2, self.jugador2.puntos)), self.marcador.posicion)
+
+                pygame.draw.line(self.pantalla, cf._BLANCO, (cf.pantalla_centro_h,cf._PANTALLA_ALTURA), (cf.pantalla_centro_h, 0), cf._ANCHO_LINEA)
+                pygame.draw.rect(self.pantalla, (cf._BLANCO), self.jugador1)
+                pygame.draw.rect(self.pantalla, (cf._BLANCO), self.jugador2)
+                pygame.draw.rect(self.pantalla, (cf._BLANCO), self.bola)
+			
             else:
-                self.jugador1.muevete()
-                self.jugador2.muevete()
-                self.bola.muevete()
-
-                self.colision_paleta()
-                self.colision_bordes()
-
-            self.pantalla.blit(self.marcador.pintar(
-                (1, self.jugador1.puntos)), self.marcador.posicion)
-            self.pantalla.blit(self.marcador.pintar(
-                (2, self.jugador2.puntos)), self.marcador.posicion)
-
-            pygame.draw.line(self.pantalla, cf._BLANCO, (cf.pantalla_centro_h,
-                             cf._PANTALLA_ALTURA), (cf.pantalla_centro_h, 0), cf._ANCHO_LINEA)
-            pygame.draw.rect(self.pantalla, (cf._BLANCO), self.jugador1)
-            pygame.draw.rect(self.pantalla, (cf._BLANCO), self.jugador2)
-            pygame.draw.rect(self.pantalla, (cf._BLANCO), self.bola)
+                color1=(25,200,20)
+                color2=(25,200,20)
+                if self.jugadores == 1:
+                    color1=(0, 255, 0)
+                    color2=(255,255,255)
+                elif self.jugadores == 2:
+                    color1=(255,255,255)
+                    color2=(0, 255, 0)					
+				#creamos el texto de "Elegir jugadores 1/2"
+                mensaje = pygame.font.SysFont(cf._ARIAL, 50)
+                texto=pygame.font.Font.render(mensaje,'Selecciona los jugadores:',True,cf._BLANCO)
+                texto_coor=(cf._PANTALLA_ANCHO/2-texto.get_width()/2,texto.get_height()+cf._PANTALLA_MARGEN_LATERAL)
+				
+				#marcamos jugadores si se seleccionan
+                uno=pygame.font.Font.render(mensaje,'1',True,cf._BLANCO,color1)
+                uno_coor=(cf._PANTALLA_ANCHO/2-uno.get_width()-cf._PANTALLA_MARGEN_LATERAL,cf._PANTALLA_ALTURA-uno.get_height()-cf._PANTALLA_MARGEN_LATERAL)
+				
+                dos=pygame.font.Font.render(mensaje,'2',True,cf._BLANCO,color2)
+                dos_coor=(cf._PANTALLA_ANCHO/2+cf._PANTALLA_MARGEN_LATERAL,cf._PANTALLA_ALTURA-uno.get_height()-cf._PANTALLA_MARGEN_LATERAL)
+				
+                self.pantalla.blit(texto,texto_coor)
+                self.pantalla.blit(uno,uno_coor)
+                self.pantalla.blit(dos,dos_coor)
+				#presionar "empezar" tecla espacio
+				
+				
             pygame.display.flip()
             self.reloj.tick(cf._JUEGO_FPS)
 
